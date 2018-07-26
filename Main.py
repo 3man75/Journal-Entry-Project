5008cda21ed7d75894d0175758e3dd80401e0269
Javier <Cadetmunoz@gmail.com>
2018-07-26 19:45:41 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,5 +3,3 @@ import datetime
 to_day = datetime.datetime.now().strftime("Date: %Y-%m-%d Time: %I:%M %p")
 
 Journal_Entry = input("What are you feeling today?")
-
-print(to_day,'\n  \n', Journal_Entry)
